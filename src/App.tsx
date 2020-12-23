e6d35ef262cefd1eba11e9b24b3f3202b1cc9103
Lincoln Nogueira <lincolnnr@gmail.com>
2020-12-23 11:01:05 -0300

    Gobarber-WEB Toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import SignIn from './pages/Signin'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// import SignUp from './pages/SignUp';
+
+import GlobalStyle from './styles/global';  
+
+import ToastContainer from './components/ToastContainer';
+import { AuthProvider } from './hooks/AuthContext';
+
+const App: React.FC = () => (
+  <>
+    <AuthProvider>
+      <SignIn />
+    </AuthProvider>
+
+    <ToastContainer />
+    <GlobalStyle /> 
+  </>
+);
 
 export default App;
